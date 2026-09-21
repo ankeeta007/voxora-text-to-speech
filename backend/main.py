73cfdade28b22616f101a6fbf57cd9d399d43eaf
ankeeta007 <ankitabaral398@gmail.com>
2026-09-21 20:54:23 +0530

    Fix CORS for deployed frontend

--- backend/main.py
+++ backend/main.py
@@ -26,9 +26,10 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+    "https://voxora-frontend-9vpt.onrender.app"   
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://voice-forge-frontend.vercel.app"
+    
 ],
     allow_credentials=True,
     allow_methods=["*"],
