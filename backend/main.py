79e718e81e0613dcca1ef4eca8adf6bcf52243c7
ankeeta007 <ankitabaral398@gmail.com>
2026-09-21 21:01:09 +0530

    Fix CORS for deployed frontend

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-    "https://voxora-frontend-9vp1.onrender.app"   
+    "https://voxora-frontend-9vp1.onrender.com"   
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     
